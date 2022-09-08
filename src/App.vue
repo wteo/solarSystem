395cb03714cba00613aa2348874a14b5e728276c
Wendy Teo <wendyteo.wy@gmail.com>
2022-09-09 06:58:26 +1000

    solar system

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<script>
+
+import Planets from './components/Planets.vue';
+
+export default {
+  components: { Planets }
+}
+
+</script>
+
+<template>
+
+  <h1>The Solar System</h1>
+  <div class="solarSystemContainer">
+    <div id="sun" />
+    <Planets />
+  </div>
+
+</template>
+
+
+<style>
+
+* {
+  box-sizing: border-box;
+  --space-background-color: rgb(22, 16, 58);
+  --orbit-border: 2px #4A437F dashed;
+  --font-color: white;
+}
+
+#body {
+  background-color: var(--space-background-color);
+}
+
+h1 {
+  position: fixed;
+  top: 20px;
+  left: 20px;
+  font-size: 300%;
+  z-index: 200;
+  color: var(--font-color);
+}
+
+.solarSystemContainer {
+  position: fixed;
+  top: 20px;
+  left: 20px;
+  height: 100%;
+  width: 100%;
+}
+
+#sun {
+  position: absolute;
+  top: 230px;
+  left: 230px;
+  width: 150px;
+  height: 150px;
+  border-radius: 100%;
+  background-color: orange;
+  z-index: 100;
+}
+
+</style>
---
+++ src/components/Planet.vue
@@ -0,0 +1,94 @@
+<script>
+
+export default {
+  data() {
+    return {
+      isClicked: false
+    }
+  },
+  props: ['description', 'planet', 'orbit', 'imgLink'],
+  methods: {
+    clickHandler() {
+      this.isClicked = !this.isClicked;
+    }
+  }
+}
+
+</script>
+
+
+<template>
+  <div v-if="isClicked" class="description_box">
+    <button class="close-button" @click="clickHandler">X</button>
+    <div class="description">
+        <h3>{{`${planet.charAt(0).toUpperCase()}${planet.slice(1)}`}}</h3>
+        <p>{{ description }}</p>
+    </div>
+  </div>
+  <div :id="orbit" class="orbit">
+    <div :id="planet" :class="{ planet : !isClicked, 'planet clicked' : isClicked }">
+      <img class="planet-image" @click="clickHandler" :src="imgLink" :alt="planet" />
+    </div>
+  </div>  
+</template>
+
+
+<style>
+
+.description {
+    position: relative;
+    top: 60px;
+    background-color: rgba(1, 77, 116, 0.7);
+    color: var(--font-color);
+    width: 500px;
+    z-index: 200;
+    border-radius: 10px;
+    padding: 3px 30px;
+}
+
+.close-button {
+    position: relative;
+    top: 85px;
+    left: 470px;
+    background-color: transparent;
+    color: var(--font-color);
+    border: 1px solid var(--font-color);
+    border-radius: 5px;
+    z-index: 250;
+    transition: background-color 0.25s;
+}
+
+.close-button:hover {
+    cursor:pointer;
+    background-color: var(--font-color);
+}
+
+
+.orbit {
+    position: absolute;
+    border: var(--orbit-border);
+    border-radius: 100%;
+}
+
+.planet-image {
+    width: 100%;
+    height: 100%;
+    border-radius: 100%;  
+}
+
+.planet {
+    position: relative;
+    border-radius: 100%;
+    transition: box-shadow 0.25s;
+}
+
+.planet:hover, .clicked {
+    cursor: pointer;
+    box-shadow: 0px 0px 20px white;
+    
+}
+
+</style>
+
+
+
