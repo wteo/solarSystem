e8c0028672cc9778e74f6429450b58b70cbca718
Wendy Teo <wendyteo.wy@gmail.com>
2022-09-13 09:31:58 +1000

    adjusted close button position

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -30,7 +30,7 @@ export default {
     top: 60px;
     background-color: rgba(1, 77, 116, 0.7);
     color: var(--font-color);
-    width: 450px;
+    width: 60%;
     z-index: 200;
     border-radius: 10px;
     padding: 3px 30px;
@@ -39,7 +39,7 @@ export default {
 .close-button {
     position: relative;
     top: 85px;
-    left: 422.5px;
+    left: 58%;
     background-color: transparent;
     color: var(--font-color);
     border: 1px solid var(--font-color);
@@ -51,6 +51,44 @@ export default {
 .close-button:hover {
     cursor:pointer;
     background-color: var(--font-color);
+}
+
+
+@media only screen and (max-width: 1200px) {
+    .close-button {
+        left: 57.5%;
+    }
+}
+
+@media only screen and (max-width: 1000px) {    
+    .close-button {
+        left: 57%;
+    }
+}
+
+@media only screen and (max-width: 850px) {    
+    .close-button {
+        left: 56.5%;
+    }
+}
+
+
+@media only screen and (max-width: 700px) {    
+    .close-button {
+        left: 56%;
+    }
+}
+
+@media only screen and (max-width: 675px) {    
+    .close-button {
+        left: 55.5%;
+    }
+}
+
+@media only screen and (max-width: 550px) {    
+    .close-button {
+        left: 54%;
+    }
 }
 
 </style>
